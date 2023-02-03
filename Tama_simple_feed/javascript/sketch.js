cebf131ce7b1feda6a47d5b99097493b42351412
Faye419 <faye_rra@qq.com>
2023-02-03 18:43:17 +0000

    new

---
+++ Tama_simple_feed/javascript/sketch.js
@@ -0,0 +1,113 @@
+let canvas;
+let button;
+
+let food = 0;
+let feeding = false;
+
+let hungry = 0;
+let full = 1;
+let tamaState = hungry;
+
+let tamaX;
+let tamaY;
+let tamaDiam;
+
+function setup() {
+  canvas = createCanvas(500, 500);
+
+  canvas.parent("sketch-container"); //move our canvas inside this HTML element
+
+  tamaX = width / 2;
+  tamaY = height / 2;
+  tamaDiam = width / 6;
+
+  addGUI();
+}
+
+function draw() {
+  background(200, 200, 250);
+
+  //Drawing
+  noStroke();
+
+  //manage state of Tama
+  if (tamaState == hungry) {
+    fill(255);
+
+    //manage switching to full state
+    if (tamaDiam > width / 4) {
+      tamaState = full;
+    }
+  } else if (tamaState == full) {
+    //full color
+    fill(0, 255, 0);
+
+    //manage returning to hungry state
+    if (tamaDiam > width / 6) {
+      if (frameCount % 2 == 0) tamaDiam--; // reduce every second frame
+    } else {
+      tamaState = hungry;
+    }
+  }
+
+  //draw Tama and closed mouth
+  circle(tamaX, tamaY, tamaDiam);
+  fill(0);
+  let mouthOffset = tamaDiam / 2;
+  ellipse(250, 265, 30, 15);
+
+  fill(0);
+  ellipse(240, 240, 10, 10);
+  ellipse(260, 240, 10, 10);
+
+  if (food > 0) {
+    //Tama Eat
+    if (frameCount % 50 < 15 && tamaState == hungry) {
+      eatFood();
+    }
+
+    //draw food
+    fill(100, 100);
+    circle(tamaX, tamaY + food, food);
+  } else if (feeding) {
+    //manage button state, only do this once IF the button is inactive
+    feeding = false;
+    button.html("FEED");
+    button.removeClass("inactive");
+  }
+}
+
+function eatFood() {
+  //draw open mouth
+  fill(256, 56, 78);
+  circle(tamaX, tamaY, tamaDiam / 2);
+
+  //reduce food & grow Tama
+  food--;
+  tamaDiam++;
+}
+
+function addGUI() {
+  //add a button
+  button = createButton("FEED");
+
+  button.addClass("button");
+
+  //Add the play button to the parent gui HTML element
+  button.parent("gui-container");
+
+  //Adding a mouse pressed event listener to the button
+  button.mousePressed(handleButtonPress);
+}
+
+function handleButtonPress() {
+  if (!feeding) {
+    //set food to random value
+    food = random(0, 60);
+    feeding = true;
+
+    //manage button state
+    button.html("FEEDING");
+    button.addClass("inactive");
+  }
+}
